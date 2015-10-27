ff46a6abd85c80bde2aece66be5afbc3add1f6a2
Javier Gómez <jgomez@velocitypartners.net>
2015-10-27 17:04:31 -0300

    Added new methods related with Accounts

--- src/main/scala/org/killbill/billing/client/KillBillClient.scala
+++ src/main/scala/org/killbill/billing/client/KillBillClient.scala
@@ -1,10 +1,12 @@
 package org.killbill.billing.client.actor
 
+import java.util.UUID
+
 import akka.actor.{ActorSystem, Props}
 import akka.event.Logging
 import akka.pattern.ask
 import akka.util.Timeout
-import org.killbill.billing.client.actor.AccountActor.{CreateAccount, GetAccountByExternalKey}
+import org.killbill.billing.client.actor.AccountActor._
 import org.killbill.billing.client.model.Account
 import spray.http._
 
@@ -25,8 +27,24 @@ class KillBillClient(killBillUrl: String, headers: List[HttpHeader with Serializ
   val accountActor = system.actorOf(Props(new AccountActor(killBillUrl, headers)), name = "AccountActor")
 
   // Public methods to connect to the KillBill API
+
+  def searchAccounts(searchKey: String, offset: Long = 0, limit: Long = 100, auditLevel: String = "NONE"): List[Any] = {
+    val future: Future[List[Any]] = ask(accountActor, SearchAccounts(searchKey, offset, limit, auditLevel)).mapTo[List[Any]]
+    Await.result(future, timeout.duration)
+  }
+
+  def getAccounts(offset: Long = 0, limit: Long = 100, auditLevel: String = "NONE"): List[Any] = {
+    val future: Future[List[Any]] = ask(accountActor, GetAccounts(offset, limit, auditLevel)).mapTo[List[Any]]
+    Await.result(future, timeout.duration)
+  }
+
   def getAccountByExternalKey(externalKey: String, withBalance: Boolean = false, withCBA: Boolean = false, audit: String = "NONE"): Any = {
     val future: Future[Any] = ask(accountActor, GetAccountByExternalKey(externalKey, withBalance, withCBA, audit)).mapTo[Any]
+    Await.result(future, timeout.duration)
+  }
+
+  def getAccountById(accountId: UUID, withBalance: Boolean = false, withCBA: Boolean = false, audit: String = "NONE"): Any = {
+    val future: Future[Any] = ask(accountActor, GetAccountById(accountId, withBalance, withCBA, audit)).mapTo[Any]
     Await.result(future, timeout.duration)
   }
 
--- src/main/scala/org/killbill/billing/client/actor/AccountActor.scala
+++ src/main/scala/org/killbill/billing/client/actor/AccountActor.scala
@@ -1,5 +1,7 @@
 package org.killbill.billing.client.actor
 
+import java.util.UUID
+
 import akka.actor.{Actor, ActorRef}
 import akka.event.Logging
 import org.killbill.billing.client.model.{Account, AccountJsonProtocol, KillbillApiResult}
@@ -11,7 +13,10 @@ import scala.util.{Failure, Success}
 
 object AccountActor {
   case class GetAccountByExternalKey(externalKey: String, withBalance: Boolean, withCBA: Boolean, audit: String)
+  case class GetAccountById(accountId: UUID, withBalance: Boolean, withCBA: Boolean, audit: String)
   case class CreateAccount(account: Account)
+  case class GetAccounts(offset: Long, limit: Long, auditMode: String)
+  case class SearchAccounts(searchKey: String, offset: Long, limit: Long, auditMode: String)
 }
 
 class AccountActor(killBillUrl: String, headers: List[HttpHeader]) extends Actor {
@@ -29,9 +34,91 @@ class AccountActor(killBillUrl: String, headers: List[HttpHeader]) extends Actor
       context.stop(self)
     }
 
+    case GetAccountById(accountId, withBalance, withCBA, audit) => {
+      getAccountById(sender, accountId, withBalance, withCBA, audit)
+      context.stop(self)
+    }
+
     case CreateAccount(account) =>
       createAccount(sender, account)
       context.stop(self)
+
+    case GetAccounts(offset, limit, auditLevel) => {
+      getAccounts(sender, offset, limit, auditLevel)
+      context.stop(self)
+    }
+
+    case SearchAccounts(searchKey, offset, limit, auditLevel) => {
+      searchAccounts(sender, searchKey, offset, limit, auditLevel)
+      context.stop(self)
+    }
+  }
+
+  def searchAccounts(originalSender: ActorRef, searchKey: String, offset: Long, limit: Long, auditLevel: String) = {
+    log.info("Searching All Accounts with searchKey=" + searchKey)
+
+    import AccountJsonProtocol._
+    import SprayJsonSupport._
+
+    val pipeline = sendReceive ~> unmarshal[List[KillbillApiResult[Account]]]
+
+    val suffixUrl = "&accountWithBalance=false&accountWithBalanceAndCBA=false&audit=" + auditLevel
+
+    val responseFuture = pipeline {
+      Get(killBillUrl+s"/accounts/search/$searchKey?offset=$offset&limit=$limit"+suffixUrl) ~> addHeaders(headers)
+    }
+    responseFuture.onComplete {
+      case Success(response) =>
+        originalSender ! response
+      case Failure(error) =>
+        originalSender ! error.getMessage
+    }
+  }
+
+  def getAccounts(originalSender: ActorRef, offset: Long, limit: Long, auditLevel: String) = {
+    log.info("Requesting All Accounts")
+
+    import AccountJsonProtocol._
+    import SprayJsonSupport._
+
+    val pipeline = sendReceive ~> unmarshal[List[KillbillApiResult[Account]]]
+
+    val suffixUrl = "&accountWithBalance=false&accountWithBalanceAndCBA=false&audit=" + auditLevel
+
+    val responseFuture = pipeline {
+      Get(killBillUrl+s"/accounts/pagination?offset=$offset&limit=$limit"+suffixUrl) ~> addHeaders(headers)
+    }
+    responseFuture.onComplete {
+      case Success(response) =>
+        originalSender ! response
+      case Failure(error) =>
+        originalSender ! error.getMessage
+    }
+  }
+
+  def getAccountById(originalSender: ActorRef, accountId: UUID, withBalance: Boolean, withCBA: Boolean, audit: String) = {
+    log.info("Requesting Account with ID: {}", accountId.toString)
+
+    import AccountJsonProtocol._
+    import SprayJsonSupport._
+
+    val pipeline = sendReceive ~> unmarshal[KillbillApiResult[Account]]
+
+    val suffixUrl = "?accountWithBalance=" + withBalance.toString + "&accountWithBalanceAndCBA=" + withCBA.toString + "&audit=" + audit
+
+    val responseFuture = pipeline {
+      Get(killBillUrl+s"/accounts/$accountId"+suffixUrl) ~> addHeaders(headers)
+    }
+    responseFuture.onComplete {
+      case Success(response) =>
+        val account = new Account(response.accountId, response.externalKey, response.accountCBA, response.accountBalance, response.name,
+          response.firstNameLength, response.email, response.billCycleDayLocal, response.currency, response.paymentMethodId, response.timeZone,
+          response.address1, response.address2, response.postalCode, response.company, response.city, response.state, response.country,
+          response.locale, response.phone, response.isMigrated, response.isNotifiedForInvoices)
+        originalSender ! account
+      case Failure(error) =>
+        originalSender ! error.getMessage
+    }
   }
 
   def getAccountByExternalKey(originalSender: ActorRef, externalKey: String, withBalance: Boolean, withCBA: Boolean, audit: String) = {
@@ -60,7 +147,6 @@ class AccountActor(killBillUrl: String, headers: List[HttpHeader]) extends Actor
   }
 
   def createAccount(originalSender: ActorRef, account: Account) = {
-
     log.info("Creating new Account...")
 
     import AccountJsonProtocol._
